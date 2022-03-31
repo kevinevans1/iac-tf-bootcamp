bfd8a55768643c3b6b94d9aa69bcc1f2287e9407
Kevin Evans <kevin.evans1@outlook.com>
2022-03-31 14:21:26 +0100

    code update

--- Terraform/modules/storage_account.tf
+++ Terraform/modules/storage_account.tf
@@ -16,6 +16,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  depends_on               = [azurerm_resource_group.exmaple]
 
   tags = {
     environment = "staging"
--- Terraform/modules/virtual_machine.tf
+++ Terraform/modules/virtual_machine.tf
@@ -12,27 +12,27 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  depends_on          = []
-  
+  location            = var.location
+  resource_group_name = var.rg_name
+  depends_on          = [azurerm_resource_group.state-demo-secure]
+
 }
 
-#Virtual Network Subent
+#Virtual Network Subnet
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
-  depends_on           = []
+  depends_on           = [azurerm_virtual_network.example]
 }
 
 #Network Security Group
 resource "azurerm_network_security_group" "example" {
   name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  depends_on          = []
+  location            = var.location
+  resource_group_name = var.rg_name
+  depends_on          = [azurerm_subnet.example]
 
   security_rule {
     name                       = "RDP"
@@ -45,73 +45,7 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-security_rule {
-    name                       = "AllowVnetInBound"
-    priority                   = 65000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "VirtualNetwork"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "AllowAzureLoadBalancerInBound"
-    priority                   = 65001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "AzureLoadBalancer"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "DenyAllInBound"
-    priority                   = 65500
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "AllowVnetOutBound"
-    priority                   = 65000
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "VirtualNetwork"
-    destination_address_prefix = "VirtualNetwork"
-  }
-  security_rule {
-    name                       = "AllowInternetOutBound"
-    priority                   = 65001
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "VirtualNetwork"
-  }
-   security_rule {
-    name                       = "DenyAllOutBound"
-    priority                   = 65500
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
+ 
   tags = {
     environment = "Dev"
   }
@@ -121,41 +55,45 @@ security_rule {
 resource "azurerm_subnet_network_security_group_association" "example" {
   subnet_id                 = azurerm_subnet.example.id
   network_security_group_id = azurerm_network_security_group.example.id
-  depends_on                = []
+  depends_on                = [azurerm_network_security_group.example]
 }
 
 #Virtual Machine Public IP
 resource "azurerm_public_ip" "public_ip" {
   name                = "vm_public_ip"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = var.rg_name
+  location            = var.location
   allocation_method   = "Dynamic"
+  depends_on = [
+    azurerm_resource_group.state-demo-secure
+  ]
 }
 
 
 #Windows Virtual Machine
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = var.rg_name
+  depends_on          = [azurerm_resource_group.state-demo-secure,azurerm_subnet.example,azurerm_public_ip.public_ip]
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
-    depends_on           = []
+
   }
- }
+}
 
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = var.rg_name
+  location            = var.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
-  depends_on          = []
+  depends_on          = [azurerm_network_interface.example,azurerm_resource_group.state-demo-secure]
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
